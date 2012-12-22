690eed96814906a93abfdfca47fd2a064f2b955c
Armen Nazaretyan <armen.nazaretyan@sflpro.com>
2012-12-22 16:54:56 +0400

    ss

---
+++ AbstractAction/src/am/tir/abstractaction/activity/ResultStories.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package am.tir.abstractaction.activity;
+
+import java.util.List;
+
+import am.tir.abstractaction.R;
+import am.tir.abstractaction.data.beans.Story;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * @author Artak Gevorgyan
+ * 
+ */
+public class ResultStories extends Activity {
+
+	private List<Story> stories;
+	private int currentIndex;
+	TextView storyText;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.result_stories);
+
+		storyText = (TextView) findViewById(R.id.story_text);
+	}
+
+	public void onNextPressed() {
+		currentIndex = currentIndex == 5 ? 0 : ++currentIndex;
+		storyText.setText(generateStoryText(stories.get(currentIndex)));
+	}
+
+	public void onPreviousPressed() {
+		currentIndex = currentIndex == 0 ? 5 : --currentIndex;
+		storyText.setText(generateStoryText(stories.get(currentIndex)));
+	}
+
+	private String generateStoryText(Story story) {
+		List<String> answers = story.getAnswers();
+
+		String result = String.format(
+				getResources().getString(R.string.story_template),
+				answers.get(0), answers.get(1), answers.get(2), answers.get(3),
+				answers.get(4), answers.get(5));
+
+		return result;
+	}
+}
