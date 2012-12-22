25874588ba9db17a1113add36645c4d196c495cf
Armen Nazaretyan <armen.nazaretyan@sflpro.com>
2012-12-22 13:28:16 +0400

    splash

---
+++ AbstractAction/src/am/tir/abstractaction/activity/Splash.java
@@ -0,0 +1,71 @@
+package am.tir.abstractaction.activity;
+
+import am.tir.abstractaction.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ImageView;
+
+/**
+ * @author Artak.Gevorgyan
+ * 
+ */
+public class Splash extends Activity {
+
+	private long ms;
+	private CountDownTimer cdt;
+	private ImageView logo;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+
+		logo = (ImageView) findViewById(R.id.ivLogo);
+
+		if (savedInstanceState != null) {
+			ms = savedInstanceState.getLong("ms");
+			if (ms < 1510) {
+				logo.setImageResource(R.integer.ss_logo);
+			}
+		} else {
+			ms = 3000;
+		}
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		cdt = new CountDownTimer(ms, 100) {
+
+			public void onTick(long millisUntilFinished) {
+				ms = millisUntilFinished;
+				if (ms < 1510 && ms > 1390) {
+					logo.setImageResource(R.integer.ss_logo);
+				}
+			}
+
+			public void onFinish() {
+				startActivity(new Intent(Splash.this, Main.class));
+				finish();
+			}
+		};
+
+		cdt.start();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putLong("ms", ms);
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	protected void onPause() {
+		cdt.cancel();
+		super.onPause();
+	}
+}
