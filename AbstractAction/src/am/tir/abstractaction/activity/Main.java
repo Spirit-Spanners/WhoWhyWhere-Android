e24b00074aab8cdcb2a92521f393bfcf3767e8e8
anazaretyan <nazaretyan.armen@gmail.com>
2012-12-22 14:22:59 +0400

    .

--- AbstractAction/src/am/tir/abstractaction/activity/Main.java
+++ AbstractAction/src/am/tir/abstractaction/activity/Main.java
@@ -2,7 +2,9 @@ package am.tir.abstractaction.activity;
 
 import am.tir.abstractaction.R;
 import android.os.Bundle;
+import android.view.View;
 import android.app.Activity;
+import android.content.Intent;
 
 public class Main extends Activity {
 
@@ -10,5 +12,10 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }    
+    } 
+    
+    public void onEnterClick(View view) {
+    	Intent intent = new Intent(this, Game.class);
+    	startActivity(intent);
+    }
 }
