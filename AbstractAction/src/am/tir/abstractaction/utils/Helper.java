eeea629e6df3253ef9fc21df76542d5ed3089a6b
anazaretyan <nazaretyan.armen@gmail.com>
2012-12-23 02:49:49 +0400

    .

--- AbstractAction/src/am/tir/abstractaction/utils/Helper.java
+++ AbstractAction/src/am/tir/abstractaction/utils/Helper.java
@@ -1,5 +1,6 @@
 package am.tir.abstractaction.utils;
 
+import android.content.Context;
 import android.graphics.Typeface;
 
 public class Helper {
@@ -7,15 +8,23 @@ public class Helper {
 	private Helper() {}
 	
 	private static Typeface hobo;
+	private static Typeface seriously;
 	
 	public static boolean isEmpty(String s) {
 		return s == null || s.trim().length() == 0;
 	}
 	
-	public static Typeface getHoboTypeface() {
+	public static Typeface getHoboTypeface(Context context) {
 		if (hobo == null) { 
-			//hobo = Typeface.createFromAsset(mgr, path)
+			hobo = Typeface.createFromAsset(context.getAssets(), "fonts/Hobo.ttf");
 		}
 		return hobo;
+	}
+	
+	public static Typeface getSeriouslyTypeface(Context context) {
+		if (seriously == null) {
+			seriously = Typeface.createFromAsset(context.getAssets(), "fonts/seriously.ttf");
+		}
+		return seriously;
 	}
 }
